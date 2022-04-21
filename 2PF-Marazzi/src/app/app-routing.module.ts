65959d04cb403c3a1cd4b10f69e9b430d0449a3a
Gabriel Marazzi <gmarazzi@gmail.com>
2022-04-21 10:57:25 -0300

    Se quitaron importaciones innecesarias

--- 2PF-Marazzi/src/app/app-routing.module.ts
+++ 2PF-Marazzi/src/app/app-routing.module.ts
@@ -3,12 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 import { HomeComponent } from './core/components/home/home.component';
-import { CoursesDetailComponent } from './featured/cursos/components/courses-detail/courses-detail.component';
-
-import { StudentsDetailComponent } from './featured/estudiantes/components/students-detail/students-detail.component';
-import { InscriptionsComponent } from './featured/inscripciones/components/inscriptions/inscriptions.component';
-
-
 
 const routes: Routes = [
   {
